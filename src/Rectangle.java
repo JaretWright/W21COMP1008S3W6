07e1f8ea9a85755907afddd7622e2a6203e305cd
Jaret Wright <hkkA223>
2021-02-24 15:47:09 -0500

    Initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,35 @@
+public class Rectangle {
+    private int length, width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getPerimeter()
+    {
+        return 2*length+2*length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        if (length>0)
+            this.length = length;
+        else
+            throw new IllegalArgumentException("length must be greater than 0");
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        if (width > 0)
+            this.width = width;
+        else
+            throw new IllegalArgumentException("width must be greater than 0");
+    }
+}
